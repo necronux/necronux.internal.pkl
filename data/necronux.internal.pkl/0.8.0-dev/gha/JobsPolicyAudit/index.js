4f34d48be9a2172e6a8a278ebe3791cbb05f3a5b
NayanTheSpaceGuy <147062173+NayanTheSpaceGuy@users.noreply.github.com>
2025-06-22 01:52:57 +0530

    Update docs

---
+++ data/necronux.internal.pkl/0.8.0-dev/gha/JobsPolicyAudit/index.js
@@ -0,0 +1,1 @@
+runtimeData.links('known-versions','[{"text":"0.8.0-dev","classes":"current-version"}]');
---
+++ data/necronux.internal.pkl/0.8.0-dev/schemas/GitHubAction/WorkflowCallSecrets.js
@@ -0,0 +1,2 @@
+runtimeData.links('known-versions','[{"text":"0.8.0-dev","classes":"current-version"}]');
+runtimeData.links('known-usages','[{"text":"WorkflowCall","href":"WorkflowCall.html"}]');
